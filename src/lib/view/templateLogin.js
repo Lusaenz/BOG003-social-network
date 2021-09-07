8103d7e273e9513c8f964a82c4d99ae323f39251
mariajossdeny <mariajossdeny@gmail.com>
2021-09-07 17:47:44 -0500

    arreglos de formulario

--- src/lib/view/templateLogin.js
+++ src/lib/view/templateLogin.js
@@ -1,11 +1,11 @@
 export const login = () => {
     const loginEmail = 
     `<section id="login-first-page" class="login-first-page">
-    <input type="email" placeholder="Correo electronico" pattern=".+@globex\.com">
-    <input type="password" placeholder="Contraseña">
-    <button>Iniciar Sesion</button>
-    <a href="#/Registrate-aqui">Registrate aqui</a> 
-    <button>Iniciar sesion con google</button>
+        <input type="email" placeholder="Correo electronico" pattern=".+@globex\.com">
+        <input type="password" placeholder="Contraseña">
+        <button>Iniciar Sesion</button>
+        <a href="#/Registrate-aqui">Registrate aqui</a> 
+        <button>Iniciar sesion con google</button>
     </section>`
     
  return loginEmail;

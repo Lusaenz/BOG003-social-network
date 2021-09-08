0f1553b062c874bcc0530d353b8de73402ccf786
leidypaez <leidy16paez@gmail.com>
2021-09-08 12:52:30 -0500

    arreglos de eslint y css

--- src/lib/router.js
+++ src/lib/router.js
@@ -1,29 +1,29 @@
+/* eslint-disable no-else-return */
 import { login } from './view/templateLogin.js';
 import { registration } from './view/templateRegistration.js';
 
-
-export const changeRouter = (hash) => {
-  if (hash === '#/') {
-    return showTemplate(hash)
-  } else if (hash === '#/Registrate-aqui') {
-    return showTemplate(hash);
-  } else {
-    return showTemplate(hash);
-  };
-};
-
 const showTemplate = (hash) => {
-  const containerRoot = document.getElementById("root");
+  const containerRoot = document.getElementById('root');
   containerRoot.innerHTML = login();
 
   switch (hash) {
     case '#/':
-      containerRoot.innerHTML = (login());
+      containerRoot.innerHTML = login();
       break;
     case '#/Registrate-aqui':
-      containerRoot.innerHTML = (registration());
+      containerRoot.innerHTML = registration();
       break;
     default:
-      containerRoot.innerText = 'Pagina no existente'
+      containerRoot.innerText = 'Pagina no existente';
+  }
+};
+
+export const changeRouter = (hash) => {
+  if (hash === '#/') {
+    return showTemplate(hash);
+  } else if (hash === '#/Registrate-aqui') {
+    return showTemplate(hash);
+  } else {
+    return showTemplate(hash);
   }
 };
--- src/main.js
+++ src/main.js
@@ -1,22 +1,18 @@
 // Este es el punto de entrada de tu aplicacion
 
 import { myFunction } from './lib/index.js';
-import{login} from './lib/view/templateLogin.js';
-import{registration} from './lib/view/templateRegistration.js';
-import{changeRouter}from'./lib/router.js';
+import { login } from './lib/view/templateLogin.js';
+import { registration } from './lib/view/templateRegistration.js';
+import { changeRouter } from './lib/router.js';
 
+document.getElementById('root').innerHTML = registration();
+const init = () => {
+  document.getElementById('root').innerHTML = login();
+  window.addEventListener('hashchange', () => {
+    myFunction();
+    console.log(window.location.hash);
+    changeRouter(window.location.hash);
+  });
+};
 
-
-document.getElementById("root").innerHTML =registration();
-
-const init =() => {
-    
-document.getElementById("root").innerHTML= login();
-    window.addEventListener('hashchange',()=>{
-        myFunction();
-        console.log(window.location.hash);
-        changeRouter(window.location.hash); 
-
-    })
-}
-window.addEventListener('load',init)
+window.addEventListener('load', init);

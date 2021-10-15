cd61a7aaf200de382aae31d1cad9d7fd659c7c83
Luisa Saenz <lsaenzroa@gmail.com>
2021-10-15 13:55:23 -0500

    Se suben cambios en la funcion de editar y queda funcional el boton de editar y el de acptar cambios

--- src/main.js
+++ src/main.js
@@ -1,13 +1,8 @@
 // Este es el punto de entrada de tu aplicacion
 
 import { myFunction } from './index.js';
-<<<<<<< HEAD
 import { login } from './lib/view/templateLogin.js';
-// import { registration } from './lib/view/templateRegistration.js';
-=======
-// import { login } from './lib/view/templateLogin.js';
 import { registration } from './lib/view/templateRegistration.js';
->>>>>>> ab6ec8c0f35197957d6dd5afb94c0c1dddaac213
 import { changeRouter } from './lib/router.js';
 
 document.getElementById('root').innerHTML = registration();
